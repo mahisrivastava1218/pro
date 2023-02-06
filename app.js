28021f4027049ff178d658691d7d3ef859517091
Mahima <98544175+mahisrivastava1218@users.noreply.github.com>
2023-02-06 21:52:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const mongoose= require('mongoose');
+const dotenv= require('dotenv');
+const app=express();
+dotenv.config({path: './config.env'})
+require('./review')
+const PORT= process.env.PORT
+
+mongoose.set('strictQuery', true);
+const middleware=(req,res,next)=>{
+    console.log('middleware')
+    next();
+}
+
+app.get('/',(req,res)=>{
+    res.send('jppp')
+})
+app.get('/about',(req,res)=>{
+    res.send('jppp')
+})
+
+app.get('/contact',middleware,(req,res)=>{
+    console.log('contact')
+    res.send('jppp')
+})
+app.get('/signin',(req,res)=>{
+    res.send('jppp')
+})
+app.get('/signup',(req,res)=>{
+    res.send('jppp')
+})
+
+app.listen(PORT,()=>{
+    console.log(`hello ${PORT}`)
+})
